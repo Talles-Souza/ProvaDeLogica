70e0a4bdd2104ea83185b7e16b6443b2cbe526c6
Talles-Souza <tallesdelfino1@gmail.com>
2023-04-12 14:33:44 -0300

    segunda pergunta pronta

---
+++ Pergunta 2/Calculo.cs
@@ -0,0 +1,30 @@
+﻿namespace Pergunta_2
+{
+    public class Calculo
+    {
+
+        public double Calcular(double a, double b, double c)
+        {
+            if (a > b && a > c)
+            {
+                double result = ((a * 4) + (b * 3) + (c * 3)) / 10;
+                return result;
+            }
+            else if (b > a && b > c)
+            {
+                double result = ((a * 3) + (b * 4) + (c * 3)) / 10;
+                return result;
+            }
+            else if (c > b && c > a)
+            {
+                double result = ((a * 3) + (b * 3) + (c * 4)) / 10;
+                return result;
+            }
+            else
+            {
+                double result = ((a * 3) + (b * 3) + (c * 3)) / 10;
+                return result;
+            }
+        }
+    }
+}
---
+++ Pergunta 2/Exercicio.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pergunta_2
+{
+    public class Exercicio
+    {
+        static void Main(string[] args)
+        {
+            double n1;
+            double n2;
+            double n3;
+            int codigo;
+            char response;
+            var calculo = new Calculo();
+
+            Console.WriteLine("Bem-vindo caro aluno, gostaria de saber sua média ponderada? (S) / (N)");
+            response = Console.ReadKey().KeyChar;
+
+            while (response == 's' || response == 'S')
+            {
+                Console.Clear();
+                Console.WriteLine("Por favor, digite o código do aluno");
+                codigo = int.Parse(Console.ReadLine());
+                if (codigo <= 0) {
+                    Console.WriteLine("Código inválido");
+                    response = 'n';
+                }
+                Console.WriteLine("Aluno com o código " + codigo + " foi encontrado com sucesso");
+                Console.WriteLine("Agora por favor, digite a sua primeira nota ");
+                n1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Agora por favor, digite a sua segunda nota ");
+                n2 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Agora por favor, digite a sua terceira nota ");
+                n3 = int.Parse(Console.ReadLine());
+
+                var result = calculo.Calcular(n1, n2, n3);
+
+                if (result < 6) {
+                    Console.WriteLine("O aluno " + codigo + " com as seguintes notas " + n1 + " " +n2+ " " + n3+ " possui a média ponderada de " +result.ToString("N2")+ " sendo assim, ele está REPROVADO, pois a média " +
+                        "mínima para aprovação é de 6 .");
+                }
+                else
+                {
+                    Console.WriteLine("O aluno " + codigo + " com as seguintes notas " + n1 + " " + n2 + " " + n3 + " possui a média ponderada de " + result + " sendo assim, ele está APROVADO .");
+                }
+                Console.WriteLine("Gostaria de consultar a média novamente?  (S) / (N)");
+                response = Console.ReadKey().KeyChar;
+                //Console.Clear() ;
+            }
+            Console.Clear();
+            Console.WriteLine("Muito obrigado por usar nossa consulta de média, e volte sempre .");
+        }
+    }
+}
