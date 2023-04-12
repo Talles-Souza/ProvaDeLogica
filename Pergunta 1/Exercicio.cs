b5feac20e2246896344627859433276823d67976
Talles-Souza <tallesdelfino1@gmail.com>
2023-04-12 00:10:29 -0300

    primeira pergunta finalizada

--- Pergunta 1/Exercicio.cs
+++ Pergunta 1/Exercicio.cs
@@ -35,6 +35,10 @@ namespace Pergunta_1
                 int carDecision = int.Parse(Console.ReadLine());
                 if (carDecision == 1)
                 {
+                    Console.Clear();
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    Console.WriteLine();
                     Console.WriteLine("** ----------------------------------------------------------------------------------------------------------------- **");
                     Console.WriteLine("   Nome : " + cars.newCar[0] + "   Ano : " + cars.newCar[1] + "   Preço : " + cars.newCar[2] + "   Desconto: "+ cars.newCar[3] + "   Total a Pagar : " + cars.newCar[4]);
                     Console.WriteLine("   Nome : " + cars.newCar2[0] + "   Ano : " + cars.newCar2[1] + "   Preço : " + cars.newCar2[2] + "     Desconto: " + cars.newCar2[3] + "     Total a Pagar : " + cars.newCar2[4]);
@@ -44,7 +48,17 @@ namespace Pergunta_1
                 }
                 else if (carDecision == 2)
                 {
-                    Console.WriteLine("Antigo");
+                    Console.Clear();
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    Console.WriteLine("** ----------------------------------------------------------------------------------------------------------------- **");
+                    Console.WriteLine("   Nome : " + cars.oldCar[0] + "       Ano : " + cars.oldCar[1] + "   Preço : " + cars.oldCar[2] + "   Desconto: " + cars.oldCar[3] + "     Total a Pagar : " + cars.oldCar[4]);
+                    Console.WriteLine("   Nome : " + cars.oldCar2[0] + "         Ano : " + cars.oldCar2[1] + "   Preço : " + cars.oldCar2[2] + "    Desconto: " + cars.oldCar2[3] + "       Total a Pagar : " + cars.oldCar2[4]);
+                    Console.WriteLine("   Nome : " + cars.oldCar3[0] + "    Ano : " + cars.oldCar3[1] + "   Preço : " + cars.oldCar3[2] + "    Desconto: " + cars.oldCar3[3] + "     Total a Pagar : " + cars.oldCar3[4]);
+                    Console.WriteLine("   Nome : " + cars.oldCar4[0] + "       Ano : " + cars.oldCar4[1] + "   Preço : " + cars.oldCar4[2] + "     Desconto: " + cars.oldCar4[3] + "     Total a Pagar : " + cars.oldCar4[4]);
+                    Console.WriteLine("** ----------------------------------------------------------------------------------------------------------------- **");
+
                 }
                 else
                 {
