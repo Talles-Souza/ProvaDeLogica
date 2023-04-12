4eb481a4f85ea28775fcde98921e2881853709f2
Talles-Souza <tallesdelfino1@gmail.com>
2023-04-12 16:16:34 -0300

    começando questao 4

---
+++ Pergunta 4/Calculo.cs
@@ -0,0 +1,28 @@
+﻿namespace Pergunta_4
+{
+    public class Calculo
+    {
+        public double judrosDia = 0.3;
+        public double multaFixa = 2.00;
+
+
+        public double Calcular()
+        {
+            return 0;
+        }
+        public int VerificarDias(DateTime vencimento, DateTime pagamento)
+        {
+            return 0;
+        }
+
+        public bool VerificaFeriado(DateTime date)
+        {
+            return false;
+        }
+
+        public bool VerificaFinalDeSemana(DateTime date)
+        {
+            return false;
+        }
+    }
+}
---
+++ Pergunta 4/Exercicio.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pergunta_4
+{
+    public class Exercicio
+    {
+        static void Main(string[] args)
+        {
+            //var formattedDate = String.Format("{0:dd/MM/yyyy}",dataPagamento);
+            DateTime dataVencimento;
+            DateTime dataPagamento;
+            string dateUser;
+            double valorBoleto;
+            double valorBoletoRecalculado;
+
+            Console.WriteLine("Digite uma data (dd/mm/yyyy): ");
+            dateUser = Console.ReadLine();
+           
+            if (DateTime.TryParse(dateUser, out dataPagamento))
+            {
+                Console.WriteLine("Por favor, digite a data do pagamento");
+                dateUser = Console.ReadLine();
+                if (DateTime.TryParse(dateUser, out dataVencimento))
+                {
+                    Console.Clear();
+                    Console.WriteLine("Agora, digite o valor de sua fatura por gentileza . ");
+                    valorBoleto = double.Parse(Console.ReadLine());
+                    Console.WriteLine();
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.WriteLine("Formato inválido: " + dateUser);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Formato inválido: " + dateUser);
+            }
+            Console.WriteLine("Obrigado e volte sempre");
+        }
+    }
+}
