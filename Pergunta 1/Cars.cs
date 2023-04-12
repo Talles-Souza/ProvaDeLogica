4cb7caebaf8609609fb19ba03ef529ae12457ba9
Talles-Souza <tallesdelfino1@gmail.com>
2023-04-11 23:54:57 -0300

    finalizando a primeira pergunta

---
+++ Pergunta 1/Cars.cs
@@ -0,0 +1,23 @@
+﻿namespace Pergunta_1
+{
+    public class Cars
+    {
+        public int totalCars = 8;
+        public int oldcars = 4;
+        public int newcars = 4;
+
+                                 // NOME       ANO        VALOR              DESCONTO         VALOR FINAL
+        public string[] newCar = { "Ferrari", "2015", "R$ 3.000.000,00", "R$ 210.000,00" , "R$ 2.790.000,00"};
+        public string[] newCar2 = { "Porsche", "2010", "R$ 108.000,00", "R$ 7.560,00","R$ 100.440,00"};
+        public string[] newCar3 = { "Polo", "2007", "R$ 26.660,00", "R$ 1.866,20", "R$ 24.794,00" };
+        public string[] newCar4 = { "Gol", "2013", "R$ 32.051,00", "R$ 2.243,57", "R$ 29.807,43" };
+
+        public string[] oldCar = { "Fusca", "1980", "R$ 22.610,00 ", "R$ 2.713,20", "R$ 19.896,80" };
+        public string[] oldCar2 = { "Gol", "1996", "R$  8.150,00", "R$ 978,00", "R$ 7.172,00" };
+        public string[] oldCar3 = { "Brasilia", "1980", "R$ 28.027,00", "R$ 3.363,24", "R$ 24.663,76" };
+        public string[] oldCar4 = { "Palio", "1997", "R$ 9.906,00", "R$ 1.188,72", "R$ 8.717,28" };
+
+
+
+    }
+}
---
+++ Pergunta 1/Exercicio.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pergunta_1
+{
+    public class Exercicio
+    {
+        static void Main(string[] args)
+        {
+            char response;
+
+            Cars cars = new Cars();
+
+            Console.WriteLine("Olá, Bem-vindo a nossa concessionária!");
+            Console.WriteLine("Deseja olhar nossos veículos ? (S/N)");
+            response = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+            Console.Clear();
+
+            while (response == 's' || response == 'S')
+            {
+                Console.WriteLine("Ótima escolha, possuimos diversos veículos, com certeza teremos " +
+                    "um que lhe agrade. ");
+
+                Console.WriteLine("Nós possuímos um total de " + cars.totalCars + " carros sendo " +
+                    cars.oldcars + " deles abaixo do ano 2000 e " +
+                    cars.newcars + " acima do ano 2000");
+                Console.WriteLine();
+                Console.WriteLine("Deseja olhar quais modelos ?" +
+                    "  (1) - Novos  (2) - Antigos");
+                int carDecision = int.Parse(Console.ReadLine());
+                if (carDecision == 1)
+                {
+                    Console.WriteLine("** ----------------------------------------------------------------------------------------------------------------- **");
+                    Console.WriteLine("   Nome : " + cars.newCar[0] + "   Ano : " + cars.newCar[1] + "   Preço : " + cars.newCar[2] + "   Desconto: "+ cars.newCar[3] + "   Total a Pagar : " + cars.newCar[4]);
+                    Console.WriteLine("   Nome : " + cars.newCar2[0] + "   Ano : " + cars.newCar2[1] + "   Preço : " + cars.newCar2[2] + "     Desconto: " + cars.newCar2[3] + "     Total a Pagar : " + cars.newCar2[4]);
+                    Console.WriteLine("   Nome : " + cars.newCar3[0] + "      Ano : " + cars.newCar3[1] + "   Preço : " + cars.newCar3[2] + "      Desconto: " + cars.newCar3[3] + "     Total a Pagar : " + cars.newCar3[4]);
+                    Console.WriteLine("   Nome : " + cars.newCar4[0] + "       Ano : " + cars.newCar4[1] + "   Preço : " + cars.newCar4[2] + "      Desconto: " + cars.newCar4[3] + "     Total a Pagar : " + cars.newCar4[4]);
+                    Console.WriteLine("** ----------------------------------------------------------------------------------------------------------------- **");
+                }
+                else if (carDecision == 2)
+                {
+                    Console.WriteLine("Antigo");
+                }
+                else
+                {
+                    Console.WriteLine("Opção inexistente - Por favor escolha corretamente os modelos");
+
+                }
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+
+                Console.WriteLine("Deseja olhar novamente os nossos veículos ? (S/N)");
+                response = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+                Console.Clear();
+            }
+            Console.Clear();
+            Console.WriteLine("Muito obrigado por ter vindo e volte sempre.");
+        }
+    }
+}
