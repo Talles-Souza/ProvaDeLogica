eaccf3ce0ace4e057f466f2285724088aa3485a8
Talles-Souza <tallesdelfino1@gmail.com>
2023-04-13 10:56:20 -0300

    terminando quesão 3

---
+++ Pergunta 3/Exercicio.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pergunta_3
+{
+    public class Exercicio
+    {
+        // As respostas a seguir, foram tiradas do algoritimo disponilibizados na prova
+        // estarei informando apenas as respostas de acordo com o exemplo fornecido
+
+
+        //Respostas
+
+        // A) - Não é possível forma um triângulo.
+        // B) - Triângulo Escaleno.
+        // C) - Não é possível forma um triângulo.
+        // D) - Triângulo Equilátero.
+        // E) - Triângulo Isósceles.
+
+    }
+}
