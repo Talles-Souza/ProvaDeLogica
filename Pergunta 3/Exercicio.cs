3dc824b1021634b9428be1dd250e1fc01e8c628f
Talles-Souza <tallesdelfino1@gmail.com>
2023-04-13 10:58:24 -0300

    ultimos ajustes

--- Pergunta 3/Exercicio.cs
+++ Pergunta 3/Exercicio.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Pergunta_3
+﻿namespace Pergunta_3
 {
     public class Exercicio
     {
